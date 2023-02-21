b4c1affdff6b9c1756314a30c79c10d5bb445344
K-Minho <alsgh9979@naver.com>
2023-02-21 10:23:39 +0900

    초기 세팅

---
+++ src/test/java/shop/mtcoding/loginjoin/LoginJoinApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.loginjoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginJoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
